5dfce4d967b32e4b42a3cf6b8f890e0b3640b177
Dean DeHart <dddehart@oakland.edu>
2016-09-30 17:42:09 -0400

    Implemented ninetyNineBottlesOfBeer function logic to print the song when called.
    The function is not yet being called, so the application is not yet working as intended. Will add call to method in next commit.

--- source/beersong/BeerSong.java
+++ source/beersong/BeerSong.java
@@ -22,7 +22,15 @@ public class BeerSong {
     /**
      * Helper function prints the actual song when called.
      */
-    public void ninetyNineBottlesOfBeer() {
-        
+    public static void ninetyNineBottlesOfBeer() {
+        for (int i = 99; i > 2; i--) {
+            System.out.println(Integer.toString(i) + " bottles of beer on the wall, " + Integer.toString(i) + " bottles of beer");
+            System.out.println("take one down, pass it around, " + Integer.toString(i - 1) + " bottles of beer on the wall.");
+        }
+        System.out.println("2 bottles of beer on the wall, 2 bottles of beer");
+        System.out.println("take one down, pass it around, 1 bottle of beer on the wall.");
+            
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer");
+        System.out.println("take one down, pass it around, no more bottles of beer on the wall.");
     }
 }
