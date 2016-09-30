69fbb0fa4e9b4aa2279bb9231ce0f27d50e482af
Dean DeHart <dddehart@oakland.edu>
2016-09-30 17:21:42 -0400

    Added class BeerSong and stubbed methods to prepare for implementation.

---
+++ source/beersong/BeerSong.java
@@ -0,0 +1,28 @@
+/* @author Dean DeHart
+ * Due 160930
+ * Assignment #2 - Improve Java skills with an assignment you wouldn't
+ * even see in a Java 1 class.
+ */
+package beersong;
+
+/**
+ *
+ * @author Dean
+ */
+public class BeerSong {
+
+    /**
+     * Main method, point of entry when running the program.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    /**
+     * Helper function prints the actual song when called.
+     */
+    public void ninetyNineBottlesOfBeer() {
+        
+    }
+}
